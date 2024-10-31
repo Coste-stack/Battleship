c3f81bf52f4919a5eba43c94f3b024fac7f8b7a9
Coste-stack <p.hajdus1122@gmail.com>
2024-10-31 18:20:21 +0100

    fix: getDropPosition now works the same for mobile and desktop

--- src/scripts/shipDragHandler.js
+++ src/scripts/shipDragHandler.js
@@ -6,28 +6,57 @@ export class ShipDragHandler {
         this.offset = { x: 0, y: 0 }; // To store the drag offset
 
         // Desktop support
-        //this.gameboardElement.addEventListener('dragover', (e) => this.#dragOver(e));
-        //this.gameboardElement.addEventListener('drop', (e) => this.#drop(e));
+        this.gameboardElement.addEventListener('dragover', (e) => this.#dragOver(e));
+        this.gameboardElement.addEventListener('drop', (e) => this.#drop(e));
     }
 
     allowShipDragging(ship) {
         ship.setAttribute('draggable', 'true');
         // Desktop support
-        //ship.addEventListener('dragstart', (e) => this.#dragStart(e, ship));
-        //ship.addEventListener('dragend', () => this.#dragEnd());
+        ship.addEventListener('dragstart', (e) => this.#dragStart(e, ship));
+        ship.addEventListener('dragend', () => this.#dragEnd());
         // Mobile support
         ship.addEventListener('touchstart', (e) => this.#touchStart(e, ship));
         ship.addEventListener('touchend', () => this.#touchEnd());
     }
 
+    /* DESKTOP SUPPORT */
+
     #dragStart(e, ship) {
+        this.startShip = ship.cloneNode(false); // store the ship's starting position
         this.currentShip = ship;
         e.dataTransfer.setData('text/plain', ship.id);
     }
 
     #dragEnd() {
-        this.currentShip = null; // Clear the current ship reference
+        // Reset offset (so that next click has new offset)
+        this.offset = { x: 0, y: 0 };
+        // Clear the ship references
+        this.startShip = null;
+        this.currentShip = null;
     }
+
+    #dragOver(e) {
+        e.preventDefault(); // Prevent default to allow dropping
+    }
+
+    #drop(e) {
+        e.preventDefault(); // Prevent default behavior
+
+        const dropPosition = this.#getDropPosition(e);
+        if (this.#isValidDrop(this.currentShip, dropPosition)) {
+            // Get 'ship' object from previous position
+            const prevX = this.startShip.style.gridColumnStart - 1;
+            const prevY = this.startShip.style.gridRowStart - 1;
+
+            const shipObj = this.gameboardObj.board[prevY][prevX].ship;
+            this.#placeShip(dropPosition, shipObj);
+        } else {
+            console.log('Invalid drop position'); // Handle invalid drop
+        }
+    }
+
+    /* MOBILE SUPPORT */
 
     #touchStart(e, ship) {
         e.preventDefault();
@@ -97,37 +126,10 @@ export class ShipDragHandler {
         this.currentShip = null;
     }
 
-    #getDropPositionTouch() {
-        const gameboardRect = this.gameboardElement.getBoundingClientRect();
-        const cellWidth = gameboardRect.width / this.gameboardObj.width;
-        const cellHeight = gameboardRect.height / this.gameboardObj.height;
+    /* OTHER FUNCTIONS */
 
-        // calculate current ship position
-        // currentShip.style.top/left to take left upper corner of ship
-        // add cellWidth/Height to take the center of left ship corner
-        const xPos = Math.floor((parseFloat(this.currentShip.style.top) + cellHeight/2) / cellHeight);
-        const yPos = Math.floor((parseFloat(this.currentShip.style.left) + cellWidth/2) / cellWidth);
-        console.log(xPos, yPos);
-
-        return { x: xPos, y: yPos };
-    }
-
-    #dragOver(e) {
-        e.preventDefault(); // Prevent default to allow dropping
-    }
-
-    #drop(e) {
-        e.preventDefault(); // Prevent default behavior
-
-        const dropPosition = this.#getDropPosition();
-        if (this.#isValidDrop(this.currentShip, dropPosition)) {
-            this.#placeShip(dropPosition);
-        } else {
-            console.log('Invalid drop position'); // Handle invalid drop
-        }
-    }
-
-    #getDropPosition() {
+    // Desktop
+    #getDropPosition(e) {
         const gameboardRect = this.gameboardElement.getBoundingClientRect();
         const cellWidth = gameboardRect.width / this.gameboardObj.width;
         const cellHeight = gameboardRect.height / this.gameboardObj.height;
@@ -138,11 +140,19 @@ export class ShipDragHandler {
         return { x: xPos, y: yPos };
     }
 
-    #isValidDrop(ship, dropPosition) {
-        // Implement logic to check if the ship can be placed at dropPosition
-        // Check if the area occupied by the ship is free, considering its size and orientation
-        // Return true if valid, false otherwise
-        return true; // Replace with actual validation logic
+    // Mobile
+    #getDropPositionTouch() {
+        const gameboardRect = this.gameboardElement.getBoundingClientRect();
+        const cellWidth = gameboardRect.width / this.gameboardObj.width;
+        const cellHeight = gameboardRect.height / this.gameboardObj.height;
+
+        // calculate current ship position
+        // currentShip.style.top/left to take left upper corner of ship
+        // add cellWidth/Height to take the center of left ship corner
+        const xPos = Math.floor((parseFloat(this.currentShip.style.left) + cellWidth/2) / cellWidth);
+        const yPos = Math.floor((parseFloat(this.currentShip.style.top) + cellHeight/2) / cellHeight);
+
+        return { x: xPos, y: yPos };
     }
 
     // Logic to visually place the ship on the gameboard
@@ -155,20 +165,28 @@ export class ShipDragHandler {
         this.currentShip.style.height = '';
         this.currentShip.style.left = '';
         this.currentShip.style.top = '';
+        console.log(dropPosition);
         
         switch(shipOrientation) {
             case 'x':
-                this.currentShip.style.gridRowStart = dropPosition.x + 1;
-                this.currentShip.style.gridRowEnd = dropPosition.x + 1;
-                this.currentShip.style.gridColumnStart = dropPosition.y + 1;
-                this.currentShip.style.gridColumnEnd = dropPosition.y + shipSize + 1;
+                this.currentShip.style.gridRowStart = dropPosition.y + 1;
+                this.currentShip.style.gridRowEnd = dropPosition.y + 1;
+                this.currentShip.style.gridColumnStart = dropPosition.x + 1;
+                this.currentShip.style.gridColumnEnd = dropPosition.x + shipSize + 1;
                 break;
             case 'y':
-                this.currentShip.style.gridRowStart = dropPosition.x + 1;
-                this.currentShip.style.gridRowEnd = dropPosition.x + shipSize + 1;
-                this.currentShip.style.gridColumnStart = dropPosition.y + 1;
-                this.currentShip.style.gridColumnEnd = dropPosition.y + 1;
+                this.currentShip.style.gridRowStart = dropPosition.y + 1;
+                this.currentShip.style.gridRowEnd = dropPosition.y + shipSize + 1;
+                this.currentShip.style.gridColumnStart = dropPosition.x + 1;
+                this.currentShip.style.gridColumnEnd = dropPosition.x + 1;
                 break;
         }
+    }
+
+    #isValidDrop(ship, dropPosition) {
+        // Implement logic to check if the ship can be placed at dropPosition
+        // Check if the area occupied by the ship is free, considering its size and orientation
+        // Return true if valid, false otherwise
+        return true; // Replace with actual validation logic
     }
 }
